f0bebfaa3230234717282ef5b95c677245de7793
SirCatington <48276063+SirCatington@users.noreply.github.com>
2025-10-22 23:23:53 +1000

    Add basic training loop Add SiameseDataset class which creates the image pairs for training

--- recognition/SiameseNetwork_46935708/dataset.py
+++ recognition/SiameseNetwork_46935708/dataset.py
@@ -1,1 +1,55 @@
 # the data loader for loading and preprocessing your data
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import pandas as pd
+import os
+import random
+
+class SiameseDataset(Dataset):
+    """
+    PyTorch Dataset for creating image pairs for a Siamese network.
+    """
+    def __init__(self, csv_path, image_dir, transform=None):
+        self.df = pd.read_csv(csv_path)
+
+        self.image_dir = image_dir
+        self.transform = transform
+
+        self.melanoma_ids = self.df[self.df['target'] == 1]['isic_id'].tolist()
+        self.normal_ids = self.df[self.df['target'] == 0]['isic_id'].tolist()
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, index):
+        
+        if random.random() < 0.5:
+            # Pair consists of same image class
+            target = 1.0
+            
+            if random.random() < 0.5:
+                img1_id, img2_id = random.sample(self.normal_ids, 2)
+            else:
+                img1_id, img2_id = random.sample(self.melanoma_ids, 2)
+        else:
+            # Pair consists of different image classes
+            target = 0.0
+            
+            img1_id = random.choice(self.normal_ids)
+            img2_id = random.choice(self.melanoma_ids)
+
+
+        img1_path = os.path.join(self.image_dir, f"{img1_id}.jpg")
+        img2_path = os.path.join(self.image_dir, f"{img2_id}.jpg")
+
+        img1 = Image.open(img1_path).convert("RGB")
+        img2 = Image.open(img2_path).convert("RGB")
+
+
+        if self.transform:
+            img1 = self.transform(img1)
+            img2 = self.transform(img2)
+            
+        return img1, img2, torch.tensor(target, dtype=torch.float32)
--- recognition/SiameseNetwork_46935708/train.py
+++ recognition/SiameseNetwork_46935708/train.py
@@ -1,3 +1,70 @@
 # The source code for training, validating, testing and saving your model. The model
 # should be imported from “modules.py” and the data loader should be imported from “dataset.py”. Make
 # sure to plot the losses and metrics during training
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+
+from modules import SiameseNetwork
+from dataset import SiameseDataset
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+LEARNING_RATE = 1e-5
+BATCH_SIZE = 16
+NUM_EPOCHS = 1
+
+IMAGE_DIR = "./data/train-image/image"
+CSV_FILE = "./data/train-metadata.csv"
+BEST_MODEL_SAVE_PATH = "siamese_best.pth"
+
+
+transformations = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor()
+])
+
+
+if __name__ == "__main__":
+    print(f"Using device: {DEVICE}")
+ 
+    # Initalise dataset loader
+    train_dataset = SiameseDataset(CSV_FILE, IMAGE_DIR, transformations)
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=4, pin_memory=True)
+
+
+   # Initalise model
+    model = SiameseNetwork().to(DEVICE)
+    criterion = nn.BCELoss()
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+    # Training Loop
+    print("Starting training...")
+    for epoch in range(NUM_EPOCHS):
+        model.train()
+        running_loss = 0.0
+
+        train_loop = tqdm(train_loader, desc=f"Epoch [{epoch+1}/{NUM_EPOCHS}] Train", leave=False)
+
+        for img1, img2, labels in train_loop:
+            img1, img2, labels = img1.to(DEVICE), img2.to(DEVICE), labels.to(DEVICE)
+            optimizer.zero_grad()
+            outputs = model(img1, img2)
+            loss = criterion(outputs.squeeze(1), labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            train_loop.set_postfix(loss=loss.item())
+
+        avg_train_loss = running_loss / len(train_loader)
+ 
+        print(f"Epoch [{epoch+1}/{NUM_EPOCHS}], Train Loss: {avg_train_loss:.4f}")
+
+    print("Training finished")
+
+    torch.save(model.state_dict(), BEST_MODEL_SAVE_PATH)
